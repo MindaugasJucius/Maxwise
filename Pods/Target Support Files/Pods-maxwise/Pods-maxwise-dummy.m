25082e186e1ae4330fc12dabf1f927d0e2e52ce5
MindaugasJucius <MindaugasJucius@icloud.com>
2019-02-03 17:52:30 +0200

    ocr working

---
+++ Pods/Target Support Files/Pods-maxwise/Pods-maxwise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maxwise : NSObject
+@end
+@implementation PodsDummy_Pods_maxwise
+@end
--- maxwise/ViewController.swift
+++ maxwise/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import TesseractOCR
 
 class ViewController: UIViewController {
 
+    let operationQueue = OperationQueue()
+    
+//    lazy var tesseract: G8Tesseract? = {
+//        let tesseract = G8Tesseract(language: "eng")
+//        tesseract?.engineMode = .tesseractCubeCombined
+//        tesseract?.pageSegmentationMode = .auto
+//        return tesseract
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let image = UIImage.init(named: "ReceiptSwiss")
+        recognize(image: image)
     }
 
+    private func recognize(image: UIImage?) {
+        guard let image = image else {
+            fatalError("No image")
+        }
+        guard let operation = G8RecognitionOperation(language: "eng",
+                                                configDictionary: nil,
+                                                configFileNames: nil,
+                                                absoluteDataPath: Bundle.main.bundlePath,
+                                                engineMode: .tesseractCubeCombined) else {
+            return
+        }
+        operation.tesseract.image = image
+        operation.tesseract.pageSegmentationMode = .auto
+        operation.recognitionCompleteBlock = { tesseract in
+            print(tesseract?.recognizedText)
+            
+        }
+        operationQueue.addOperation(operation)
+//        operation?.tesseract.re
+//        tesseract?.image = image//.g8_blackAndWhite()
+//        tesseract?.recognize()
+//        print(tesseract?.recognizedText)
+    }
 
 }
 
