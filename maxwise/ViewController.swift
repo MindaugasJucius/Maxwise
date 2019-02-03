f85f6addcf155464e43e85f44f080cbeafcf3afc
MindaugasJucius <MindaugasJucius@icloud.com>
2019-02-03 15:13:46 +0200

    Initial Commit

---
+++ maxwise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  maxwise
+//
+//  Created by Mindaugas Jucius on 2/3/19.
+//  Copyright © 2019 Mindaugas Jucius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
