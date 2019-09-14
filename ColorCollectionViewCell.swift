e222e90e9daca0d6a6e6832f97bfd6982b216da9
MindaugasJucius <MindaugasJucius@icloud.com>
2019-09-14 13:54:52 +0300

    add selected view to color cell

--- ColorCollectionViewCell.swift
+++ ColorCollectionViewCell.swift
@@ -1,16 +1,48 @@
 import UIKit
 
 class ColorCollectionViewCell: UICollectionViewCell {
+    
+    override var isSelected: Bool {
+        didSet {
+            selectionView.isHidden = !isSelected
+        }
+    }
+    
+    private lazy var selectionView: UIView = {
+        let view = UIView.init(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let configuration = UIImage.SymbolConfiguration.init(weight: .medium)
+        let image = UIImage.init(systemName: "checkmark",
+                                 withConfiguration: configuration)
+        
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.tintColor = .white
+        view.addSubview(imageView)
+        let constraints = [
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 20),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        
+
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        return view
+    }()
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.gray.cgColor
+        addSubview(selectionView)
+        selectionView.fillInSuperview()
+        selectionView.clipsToBounds = true
+        selectionView.isHidden = true
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = frame.width / 2
     }
-    
 }
