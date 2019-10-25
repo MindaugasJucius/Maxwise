3fb6da6ef74c21b283f2ed12411232d75bcef47b
MindaugasJucius <mindaugas.jucius@tesonet.com>
2019-10-25 12:42:43 +0300

    adjust marker offset when selected first or last circle

--- Stats/Charts/LineChartMarkerView.swift
+++ Stats/Charts/LineChartMarkerView.swift
@@ -8,17 +8,8 @@ class LineChartMarkerView: MarkerView {
     @IBOutlet weak var separatorView: UIView!
 
     private let yOffset: CGFloat = 10
-    
-    override var offset: CGPoint {
-        get {
-            return CGPoint.init(x: -(frame.width / 2),
-                                y: -frame.height - yOffset)
-        }
-        set {
-            
-        }
-    }
-    
+    private let additionalOffsetWhenOutOfBounds: CGFloat = 10
+
     override func awakeFromNib() {
         super.awakeFromNib()
         dateLabel.font = .systemFont(ofSize: 12, weight: .medium)
@@ -33,8 +24,38 @@ class LineChartMarkerView: MarkerView {
         layer.applyShadow(color: shadowColor)
         layer.applyBorder()
         layer.borderColor = UIColor.clear.cgColor
+        layer.masksToBounds = false
         
         translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    override func offsetForDrawing(atPoint point: CGPoint) -> CGPoint {
+        guard let chart = chartView else { return self.offset }
+        
+        var offset = self.offset
+        
+        let width = self.bounds.size.width
+        let height = self.bounds.size.height
+        
+        if point.x + offset.x < 0.0  // jei su dabartiniu offsetu < chart width
+        {
+            offset.x = -point.x + additionalOffsetWhenOutOfBounds
+        }
+        else if point.x + width + offset.x > chart.bounds.size.width // jei su dabartiniu offsetu > chart width
+        {
+            offset.x = chart.bounds.size.width - point.x - width - additionalOffsetWhenOutOfBounds
+        }
+        
+        if point.y + offset.y < 0
+        {
+            offset.y = -point.y
+        }
+        else if point.y + height + offset.y > chart.bounds.size.height // jei su dabartiniu offsetu islenda uz y boundu
+        {
+            offset.y = chart.bounds.size.height - point.y - height
+        }
+        
+        return offset
     }
     
     override class func viewFromXib(in bundle: Bundle = .main) -> LineChartMarkerView? {
@@ -44,16 +65,20 @@ class LineChartMarkerView: MarkerView {
     }
     
     override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
-        print(highlight.dataIndex)
-//        chartView?.data?.dataSets.first?.entryCount
         guard let formattedEntry = entry.data as? FormattedLineChartEntry else {
             return
         }
+
         dateLabel.text = formattedEntry.monthDayRepresentation
         amountLabel.text = formattedEntry.totalAmountSpent
 
         setNeedsLayout()
         layoutIfNeeded()
+
+        offset = CGPoint(
+            x: -(frame.width / 2),
+            y: -frame.height - yOffset
+        )
     }
     
 }
