6cb334bb90f0dece22af1137436beadac8c07486
MindaugasJucius <MindaugasJucius@icloud.com>
2019-11-11 22:01:22 +0200

    bump version add ui tests, fastlane

--- Pods/Target Support Files/Pods-maxwise/Pods-maxwise-frameworks.sh
+++ Pods/Target Support Files/Pods-maxwise/Pods-maxwise-frameworks.sh
@@ -162,14 +162,14 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm-iOS13.0/Realm.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift-iOS13.0/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UPCarouselFlowLayout/UPCarouselFlowLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm-iOS13.0/Realm.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift-iOS13.0/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UPCarouselFlowLayout/UPCarouselFlowLayout.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
